4bca2d2e3e0650d56542844a825b929ff8011857
Daniel070999 <danielixo99@gmail.com>
2023-04-27 21:53:22 -0500

    creacion de diseño para la seccion de projects

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as Isotope from 'isotope-layout';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.css']
 })
-export class ProjectsComponent {
+
+export class ProjectsComponent implements OnInit {
+
+  images: any = [
+    {
+      img: 'https://cutesolution.com/tf/maslin/light/assets/img/projects/work-6.jpg',
+      alt: 'webdesign',
+      value: 10
+    },
+    {
+      img: 'https://example.com/image2.jpg',
+      alt: 'Descripción de la imagen 2',
+      value: 20
+    },
+    {
+      img: 'https://example.com/image3.jpg',
+      alt: 'Descripción de la imagen 3',
+      value: 30
+    }
+  ];
+
+  ngOnInit(): void {
+
+
+    $(window).on('load', function () {
+      var projectIsotope = $('.project-container').isotope({
+        itemSelector: '.project-grid-item'
+      });
+
+      $('#project-flters li').on('click', function () {
+        $("#project-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        projectIsotope.isotope({
+          filter: $(this).data('filter')
+        });
+      });
+    });
+  }
 
 }
